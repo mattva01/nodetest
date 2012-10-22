8d63107a4059b0c243ea9c356392e138321692ad
Matthew Gallagher <mattva01@gmail.com>
2012-10-22 06:14:45 -0400

    fixed indentation

--- test.js
+++ test.js
@@ -1,38 +1,39 @@
 var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+, io = require('socket.io').listen(app)
+, fs = require('fs')
 
 app.listen(80);
 var users =[];
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
     }
 
     res.writeHead(200);
     res.end(data);
-  });
+});
 }
 
 io.sockets.on('connection', function (socket) {
-       socket.on('newuser', function (username)	{
-       socket.username = username;
-       users.push(username);
-       socket.emit("adminmessage","CONNECTED")
-       socket.broadcast.emit("adminmessage",username+" CONNECTED");
-       });
 
-socket.on('disconnect', function(){
-		socket.broadcast.emit('adminmessage', socket.username + ' has disconnected');
+   socket.on('newuser', function (username)	{
+     socket.username = username;
+     users.push(username);
+     socket.emit("adminmessage","CONNECTED")
+     socket.broadcast.emit("adminmessage",username+" CONNECTED");
+ });
 
-});
+   socket.on('disconnect', function(){
+      socket.broadcast.emit('adminmessage', socket.username + ' has disconnected');
 
-socket.on('sendchat', function (data) {
-    io.sockets.emit('clientmessage', socket.username, data);
-});
+  });
+
+   socket.on('sendchat', function (data) {
+      io.sockets.emit('clientmessage', socket.username, data);
+  });
 
 });
 
